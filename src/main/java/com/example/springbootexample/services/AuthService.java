db59b19bb43975565b6df82fbcbd74f24932bd1c
tupt <tupt@rikkeisoft.com>
2022-08-05 16:03:30 +0700

    done auth

--- src/main/java/com/example/springbootexample/services/AuthService.java
+++ src/main/java/com/example/springbootexample/services/AuthService.java
@@ -2,6 +2,7 @@ package com.example.springbootexample.services;
 
 import com.example.springbootexample.dto.ApiRepository;
 import com.example.springbootexample.dto.UserDTO;
+import com.example.springbootexample.jwt.WebSecurityConfig;
 import com.example.springbootexample.repositorys.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,12 @@ public class AuthService  {
     @Autowired
     CommonService commonService;
 
+    @Autowired
+    WebSecurityConfig webSecurityConfig;
     public ApiRepository createUser(UserDTO user) {
         ApiRepository apiRepository = new ApiRepository();
         try {
-            userRepository.saveUser(user.getUsername(),user.getPassword());
+            userRepository.saveUser(user.getUsername(),webSecurityConfig.passwordEncoder().encode(user.getPassword()).toString());
             apiRepository.setSuccess(true);
             apiRepository.setData(null);
             apiRepository.setMessage("Create user success");
